6e6584a0eb8bd091802ecdb590b2a7a259262d1c
Sahid <sahid2barroso@gmail.com>
2026-02-09 08:25:17 -0600

    CAMBIOS

--- app/config.ts
+++ app/config.ts
@@ -1,15 +1,15 @@
 export const metaData = {
-  baseUrl: "https://edubedo.com/",
-  title: "Eduardo Yair Hernández Escobedo",
+  baseUrl: "",
+  title: "",
   name: "Sirius",
   ogImage: "/opengraph-image.png",
   description: "Personal Portfolio",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/home",
-  github: "https://github.com/Eduardo Yair Hernández Escobedo",
-  instagram: "https://www.instagram.com/Eduardo Yair Hernández Escobedoo/",
-  linkedin: "https://www.linkedin.com/in/eduardo-escobedo-985904277/",
-  email: "mailto:ezescobedo27@gmail.com",
+  twitter: "",
+  github: "",
+  instagram: "",
+  linkedin: "",
+  email: "",
 };
